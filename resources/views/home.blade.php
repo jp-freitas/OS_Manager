976622c480c7924fc52ecdddccf683c9ddab6708
TecJP <tec.joao.freitas@gmail.com>
2020-03-18 10:40:56 -0300

    Alterações no layout da tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,47 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card shadow">
+        <div class="col-md-3 text-center card-user">
+            <div class="card">
+                <div class="align-items-center">
+                    <i class="fas fa-user-tie fa-5x" style="background-color: red;"></i>
+                    <h3>{{ Auth::user()->name }}</h3>
+                </div>
+                <div>
+                    @guest
+                    @else
+                    {{ ' '.__('roles.'.Auth::user()->roles->first()->name) . ' | ' }}
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('Sair') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    @endguest
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 card-actions">
+            <div class="card">
                 <div class="card-header">
-                    Gerencimando de Ordem de Serviço
+                    Gerenciamento do Sistema
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <a href="{{ route('create') }}" title="Abrir OS"><i class="far fa-plus-square fa-2x"></i></a>
-                            <a href="{{ route('regist') }}" title="Registrar OS" class="col-2"><i class="fas fa-list-ul fa-2x"></i></a>
-                            @if(Auth::user()->roles->first()->name === 'Admin')
-                            <a href="{{ route('register') }}" title="Novo Usuário"><i class="fas fa-user-plus fa-2x"></i></a>
-                            @endif
-                        </div>
-                    </div>
+                    <a href="{{ route('create') }}" title="Abrir OS"><i class="far fa-plus-square fa-2x"></i></a>
+                    <a href="{{ route('regist') }}" title="Registrar OS" class="col-2"><i class="fas fa-list-ul fa-2x"></i></a>
+                    @if(Auth::user()->roles->first()->name === 'Admin')
+                    <a href="{{ route('register') }}" title="Novo Usuário"><i class="fas fa-user-plus fa-2x"></i></a>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
     <br>
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card shadow">
+        <div class="col-md-12 card-list">
+            <div class="card">
                 <div class="card-header">
                     Lista de OS
                 </div>
                 <div class="card-body table-responsive align-middle">
-                    <!-- <div class="search">
-                        <form action="{{ route('home') }}" method="get" class="container-fluid">
-                            <div class="row mb-3">
-                                <div class="col-12">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Pesquise por departamento. Ex: ADM" name="search" value="{{ isset($search) ? $search : '' }}">
-                                        <button class="btn btn-success" type="submit">
-                                            <i class="fas fa-search"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div> -->
                     <div class="os_lists">
                         <table id="table_os" class="table table-striped table-bordered" style="width: 100%">
                             <thead>
