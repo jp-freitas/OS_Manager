6b9b9b0f2ad25dec17ef72a45676719fd7e1a20f
TecJP <tec.joao.freitas@gmail.com>
2020-03-19 12:12:37 -0300

    Remoção do card-header nos cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-3 text-center">
-            <div class="card card-user">
+        <div class="col-md-3 text-center col-sm-12">
+            <div class="card card-user shadow-sm">
                 <div class="user">
                     <i class="fas fa-user fa-3x"></i>
                 </div>
@@ -12,11 +12,10 @@
                     <h5>{{ Auth::user()->name }}</h5>
                     @guest
                     @else
-                    {{ ' '.__('roles.'.Auth::user()->roles->first()->name) . ' | ' }}
+                    {{ ' '.__('roles.'.Auth::user()->roles->first()->name) . ' ' }}
                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         {{ __('Sair') }}
                     </a>
-
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
@@ -24,12 +23,11 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-9">
-            <div class="card card-actions">
-                <div class="card-header">
-                    <h5>Gerenciamento do Sistema</h5>
-                </div>
+        <div class="col-md-9 col-sm-12">
+            <div class="card card-actions shadow-sm">
                 <div class="card-body">
+                    <h5>Gerenciamento do Sistema</h5>
+                    <br>
                     <a href="{{ route('create') }}" title="Abrir OS"><i class="fas fa-plus-square fa-3x"></i></a>
                     <a href="{{ route('regist') }}" title="Registrar OS" class="col-2"><i class="fas fa-list-ul fa-3x"></i></a>
                     @if(Auth::user()->roles->first()->name === 'Admin')
@@ -42,11 +40,10 @@
     <br>
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card card-list">
-                <div class="card-header">
-                    <h5>Lista de OS</h5>
-                </div>
+            <div class="card card-list shadow-sm">
                 <div class="card-body table-responsive align-middle">
+                    <h5>Lista de OS</h5>
+                    <br>
                     <div class="os_lists">
                         <table id="table_os" class="table table-striped table-bordered" style="width: 100%">
                             <thead>
